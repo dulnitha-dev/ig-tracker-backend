8a1ae1cbeae1a66b4043037bc6571dfc1e0a616d
dulnitha <dulnithamethdam@gmail.com>
2024-04-22 19:32:54 +0530

    get request ip

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,7 @@ const router = require("express").Router();
 const checkout = require("./checkout");
 const token = require("./token");
 const planDetails = require("../utils/planDetails");
-
-const { Logtail } = require("@logtail/node");
 const { sendEmail } = require("../utils/sendEmail");
-const logtail = new Logtail("sRBk6hMoi8YQBW6CKanunY2Z");
 
 router.get("/", (req, res) => {
   res.render("index", { title: "Homepage", plans: planDetails });
@@ -22,12 +19,7 @@ router.use("/token", token);
 
 router.get("/plans", async (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.headers["x-real-ip"];
-  if (ip) {
-    logtail.info(`Got Request Ip ${ip}`);
-    logtail.flush();
-
-    await sendEmail("IG Tracker", "dulnithamethdam@gmail.com", "Got Request Ip", `RequestIp: ${ip}`);
-  }
+  if (ip) await sendEmail("IG Tracker", "dulnitha.dev@gmail.com", "Got Request Ip", `RequestIp: ${ip}`);
   res.json(planDetails);
 });
 
